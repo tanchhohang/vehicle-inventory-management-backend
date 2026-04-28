3306ece4cc4005a0acfd781a8a60ff4abf79c779
tanchho <tanchho.limbu25@gmail.com>
2026-04-29 00:17:37 +0545

    feat(sale): reorganize .gitignore and update Sale and SaleItem models to accomodate email invoice

--- vehicle-management-backend/Models/Sale.cs
+++ vehicle-management-backend/Models/Sale.cs
@@ -4,11 +4,13 @@ public class Sale
 {
     public long Id { get; set; }
     public long CustomerId { get; set; }
+    public Users Customer { get; set; } = null!;
     public long StaffId { get; set; }
     public DateTime SaleDate { get; set; } = DateTime.UtcNow;
     public decimal SubTotal { get; set; }
     public decimal DiscountAmount { get; set; }
     public decimal TotalAmount { get; set; }
     public bool LoyaltyDiscountApplied { get; set; }
+    public bool IsPaid { get; set; } = true;
     public List<SaleItem> SaleItems { get; set; } = new();
 }
--- vehicle-management-backend/Models/SaleItem.cs
+++ vehicle-management-backend/Models/SaleItem.cs
@@ -1,9 +1,11 @@
 namespace vehicle_management_backend.Models;
+using System.Text.Json.Serialization;
 
 public class SaleItem
 {
     public long Id { get; set; }
     public long SaleId { get; set; }
+    [JsonIgnore]
     public Sale Sale { get; set; } = null!;
     public long PartId { get; set; }
     public Part Part { get; set; } = null!;
